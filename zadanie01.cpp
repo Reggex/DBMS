9e8527bc13c302766f707942f3eaec6565d82312
Reggex <sedyhnastenka@mail.ru>
2021-11-22 15:19:05 +0300

    Редактирование задания01
    1) Добавление документации
    2) Функции только для назначения
    3) Использование size_t

--- zadanie01.cpp
+++ zadanie01.cpp
@@ -4,17 +4,29 @@
 
 using namespace std;
 
-void maxElement(char* array, int size)
+/**
+	 * \brief Инициализация размера массива.
+	 * \param string Текст вводного сообщения.
+	 * \return Размер массива.
+	 */
+size_t get_size(const string& message)
 {
-	char max_element = array[0];
-	for (int i = 0; i < size; ++i) {
-		if (array[i] > max_element) {
-			max_element = array[i];
-		}
+	int size;
+	cout << message <<endl;
+	cin >> size;
+	while (size < 0) {
+		cout << "Incorrect size. Value has to be greater or equal zero." << endl;
+		cin >> size;
 	}
-	cout << "Max element of array is ' " << max_element << " '" << endl;
+	
+	return size;
 }
 
+/**
+	 * \brief Заполнение массива рандомными значениями.
+	 * \param array Массив.
+	 * \param size Размер массива.
+	 */
 void fillArray(char* array, int size) {
 	char random_char;
 	unsigned seed = time(0);
@@ -25,6 +37,11 @@ void fillArray(char* array, int size) {
 	}
 }
 
+/**
+	 * \brief Вывод массива на экран.
+	 * \param array Массив.
+	 * \param size Размер массива.
+	 */
 void printArray(char* array, int size) {
 	cout << "Your array:" << endl;
 	for (int i = 0; i < size; ++i) {
@@ -33,15 +50,38 @@ void printArray(char* array, int size) {
 	cout << endl;
 }
 
+/**
+	 * \brief Находит максимальный элемент в массиве.
+	 * \param array Массив.
+	 * \param size Размер массива.
+	 * \return Максимальный элемент.
+	 */
+char getMaxElement(char* array, int size)
+{
+	char max_element = array[0];
+	for (int i = 0; i < size; ++i) {
+		if (array[i] > max_element) {
+			max_element = array[i];
+		}
+	}
+	return max_element;
+}
+
+/**
+	 * \brief Главная программа.
+	 * \return 0 в случае успеха.
+	 */
 int main() {
-	cout << "Input size of the array, what you want." << endl;
 	int size;
-	cin >> size;
+	size = get_size("Input size of the array, what you want.");
 	char* array = new char [size];
 	fillArray(array, size);
 	printArray(array, size);
-	maxElement(array, size);
-	delete[] array;
-	system("pause");
+	cout << "Max element of array is ' " << getMaxElement(array, size) << " '" << endl;
+	if (array != nullptr)
+	{
+		delete[] array;
+		array = nullptr;
+	}
 	return 0;
 }
